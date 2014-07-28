770cc419ff1679a5f25cfbd63e392fc50b634206
ZhiHong Chang <ublue724@gmail.com>
2014-07-28 22:19:08 +0800

    Bump to v0.1.0

---
+++ devtools.js
@@ -0,0 +1,8 @@
+"use strict";
+chrome.devtools.panels.create("Live Style",
+    "css.png",
+    "editor.html",
+    function(panel) {
+      // code invoked on panel creation
+    }
+);
---
+++ editor.js
@@ -0,0 +1,27 @@
+"use strict";
+var textarea = $('#editor')[0];
+var editor = CodeMirror.fromTextArea(textarea, {
+    className: "css-editor",
+    mode: "css",
+    matchBrackets: true,
+    lineNumbers: true,
+    lineWrapping: true,
+    autoCloseBrackets: true,
+    autofocus: true,
+    styleActiveLine: true
+});
+
+editor.setSize('100%', '100%');
+
+$(editor.display.wrapper).addClass('live-css-editor');
+editor.on('change', function() {
+    chrome.devtools.inspectedWindow.eval(
+        "var __live_css_edit__ = document.querySelectorAll('#__live_css_edit__')[0];\
+       if(!__live_css_edit__) {\
+          __live_css_edit__ = document.createElement('style');\
+          __live_css_edit__.setAttribute('id', '__live_css_edit__');\
+          document.querySelectorAll('body')[0].appendChild(__live_css_edit__);\
+       }\
+       __live_css_edit__.innerHTML = '" + editor.getValue().replace(/\n/g, ' ') + "';"
+    );
+});
