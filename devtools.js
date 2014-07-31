46ed327a9ff35bf4ba8979b85eb1391ff9486a83
ZhiHong Chang <ublue724@gmail.com>
2014-07-31 21:32:27 +0800

    using base64 sync style

--- devtools.js
+++ devtools.js
@@ -3,4 +3,5 @@ chrome.devtools.panels.elements.createSidebarPane("LiveCSS",
     function(sidebar) {
         // sidebar initialization code here
         sidebar.setPage('editor.html');
+        sidebar.setHeight("300px");
     });
--- editor.js
+++ editor.js
@@ -22,13 +22,14 @@ editor.on('change', function() {
           __live_css_edit__.setAttribute('id', '__live_css_edit__');\
           document.querySelectorAll('body')[0].appendChild(__live_css_edit__);\
        }\
-       __live_css_edit__.innerHTML = '" + editor.getValue().replace(/\n/g, '<br>') + "'.replace(/<br>/g, '\\n');"
-    );
+       var __live_css_edit_style__ = '"
+       + btoa(editor.getValue())
+       + "'; __live_css_edit__.innerHTML = atob(__live_css_edit_style__);");
 });
 
 chrome.devtools.inspectedWindow.eval(
     "document.querySelectorAll('#__live_css_edit__')[0].innerHTML", function(res) {
-        editor.setValue(res);
+        editor.setValue(res || "");
         editor.focus();
     }
 );
